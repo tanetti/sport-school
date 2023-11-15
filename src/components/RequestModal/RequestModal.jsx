817a318b26e1accf7c12287438659b4049be2ce5
Volodymyr Dudka <tanetti22@gmail.com>
2023-11-15 18:42:37 +0200

    Add step switch logic

--- src/components/RequestModal/RequestModal.jsx
+++ src/components/RequestModal/RequestModal.jsx
@@ -1,11 +1,47 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 import { Modal } from '@/components/shared';
+import { TRANSITION_STANDART_DURATION_MS } from '@/constants';
+import { StepContainer } from './RequestModal.styled';
 
-export const RequestModal = ({ isOpened, closeModal }) => (
-  <Modal isOpened={isOpened} closeModal={closeModal} title="Запит запису">
-    123
-  </Modal>
-);
+export const RequestModal = ({ isOpened, closeModal }) => {
+  const [step, setStep] = useState(1);
+  const [isStepVisible, setIsStepVisible] = useState(true);
+
+  const onModalClose = () => {
+    setStep(1);
+
+    closeModal();
+  };
+
+  const onStepChange = stepNumber => {
+    setIsStepVisible(false);
+
+    setTimeout(() => {
+      setStep(stepNumber);
+
+      setIsStepVisible(true);
+    }, TRANSITION_STANDART_DURATION_MS);
+  };
+
+  return (
+    <Modal isOpened={isOpened} closeModal={onModalClose} title="Запит запису">
+      <StepContainer isStepVisible={isStepVisible}>
+        {step === 1 ? (
+          <button type="button" onClick={() => onStepChange(2)}>
+            next
+          </button>
+        ) : null}
+
+        {step === 2 ? (
+          <button type="button" onClick={() => onStepChange(1)}>
+            previous
+          </button>
+        ) : null}
+      </StepContainer>
+    </Modal>
+  );
+};
 
 RequestModal.propTypes = {
   isOpened: PropTypes.bool.isRequired,
--- src/components/RequestModal/RequestModal.styled.js
+++ src/components/RequestModal/RequestModal.styled.js
@@ -1,2 +1,9 @@
-// import styled from '@emotion/styled';
-// import { createTransition } from '@/utilities';
+import styled from '@emotion/styled';
+import { createTransition } from '@/utilities';
+
+export const StepContainer = styled.div`
+  opacity: ${({ isStepVisible }) => (isStepVisible ? 1 : 0)};
+  visibility: ${({ isStepVisible }) => (isStepVisible ? 'visible' : 'hidden')};
+
+  transition: ${createTransition(['opacity', 'visibility'], 'standart')};
+`;
