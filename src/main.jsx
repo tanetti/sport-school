2fbd9291ce5aeb7c17cf7880e4a816a7b1df11cc
Volodymyr Dudka <tanetti22@gmail.com>
2023-11-21 18:20:45 +0200

    Disable react tools on prod

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
+/* eslint-disable no-undef */
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 import 'modern-normalize';
 import './index.css';
 import { App } from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+if (process.env.NODE_ENV === 'production') {
+  disableReactDevTools();
+}
+
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
