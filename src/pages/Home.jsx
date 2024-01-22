a2daf6109499052b9141c7ca680361964f674b2d
Volodymyr Dudka <tanetti22@gmail.com>
2024-01-23 00:08:45 +0200

    Update imports

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
 import { useEffect } from 'react';
 import { Slider } from '@/components/shared';
 import { HomeSections } from '@/components';
-import { HOME_SLIDER_SETTINGS } from '@/constants';
 import { setPageTags } from '@/utilities';
+import { HOME_SLIDER_SETTINGS } from '@/constants';
 
 const Home = () => {
   useEffect(() => {
