64adb43aba840afd2b2bb74395959c682478920c
Volodymyr Dudka <tanetti22@gmail.com>
2024-02-16 10:13:30 +0200

    Fix DEV mode detection

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
-/* eslint-disable no-undef */
 import { createRoot } from 'react-dom/client';
 import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 import 'modern-normalize';
 import './index.css';
 import { App } from './App';
 
-if (process.env.NODE_ENV === 'production') {
-  disableReactDevTools();
-}
+const isDevMode = import.meta.env.DEV;
+
+if (!isDevMode) disableReactDevTools();
 
 createRoot(document.getElementById('root')).render(<App />);
